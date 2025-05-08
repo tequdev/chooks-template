9a1f9a9a136ce9535715a9b3394c6120374513a6
tequ <git@tequ.dev>
2025-05-08 13:15:56 +0900

    functional hooks

--- contracts/index.c
+++ contracts/index.c
@@ -1,14 +1,62 @@
 /**
  * This hook just accepts any transaction coming through it
  */
-#include "hookapi.h"
+#include "utils/hookapi.h"
 
-int64_t hook(uint32_t reserved) {
+#define UINT8 16
+#define UINT16 1
+#define UINT32 2
+#define UINT64 3
+#define UINT128 4
+#define UINT256 5
+#define AMOUNT 6
+#define VL 7
+#define ACCOUNT 8
 
-  TRACESTR("index.c: Called.");
-  accept(SBUF("accept"), __LINE__);
+int64_t increment(uint32_t reserved) {
+  uint8_t key[256];
+  uint32_t key_len = otxn_func_param(SBUF(key), 0, VL);
+
+  uint8_t increment_amount;
+  otxn_func_param(SVAR(increment_amount), 1, UINT8);
+
+  uint32_t amount;
+  state(SVAR(amount), key, key_len);
+  amount += increment_amount;
+
+  state_set(SVAR(amount), key, key_len);
 
   _g(1, 1);
-  // unreachable
-  return 0;
+  return accept(SBUF("accept"), __LINE__);
+}
+
+int64_t decrement(uint32_t reserved) {
+  uint8_t key[256];
+  uint32_t key_len = otxn_func_param(SBUF(key), 0, VL);
+
+  uint8_t decrement_amount;
+  otxn_func_param(SVAR(decrement_amount), 1, UINT8);
+
+  uint32_t amount;
+  state(SVAR(amount), key, key_len);
+  amount -= decrement_amount;
+
+  state_set(SVAR(amount), key, key_len);
+
+  _g(1, 1);
+  return accept(SBUF("accept"), __LINE__);
+}
+
+int64_t queryValue(uint32_t reserved) {
+  uint8_t key[256];
+  int32_t key_len = otxn_func_param(SBUF(key), 0, VL);
+  TRACEVAR(key_len);
+
+  uint32_t amount;
+  trace_num(SBUF("state"), state(SVAR(amount), key, key_len));
+
+  query_result_set(SBUF("current_amount"), SVAR(amount), UINT32);
+
+  _g(1, 1);
+  return accept(SBUF("accept"), __LINE__);
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,4 +1,4 @@
-import { SetHookFlags, calculateHookOn } from 'xahau'
+import { SetHookFlags, calculateHookOn, convertStringToHex, decode, decodeAccountID, encodeAccountID } from 'xahau'
 
 import {
   serverUrl,
@@ -6,7 +6,6 @@ import {
   setupClient,
   teardownClient,
 } from '@transia/hooks-toolkit/dist/npm/src/libs/xrpl-helpers'
-import { compileC } from '@xahau/hooks-cli'
 
 import {
   type SetHookParams,
@@ -18,25 +17,66 @@ import {
   clearHookStateV3,
   Xrpld,
 } from '@transia/hooks-toolkit'
+import { HookFunctionFlags } from 'xahau/dist/npm/models/transactions/setHook'
 
 const namespace = 'namespace'
+
+const TypeParameter = (name: string, type: string) => ({
+  FunctionParameter: {
+    FunctionParameterName: convertStringToHex(name),
+    FunctionParameterType: { type },
+  }
+})
+const ValueParameter = (type: string, value: any) => ({
+  FunctionParameter: {
+    FunctionParameterValue: { type, value: type === 'VL' ? convertStringToHex(value) : value },
+  }
+})
 
 describe('test', () => {
   let testContext: XrplIntegrationTestContext
 
   beforeAll(async () => {
-    await compileC('./contracts/index.c', 'build/')
     testContext = await setupClient(serverUrl)
     const hook = {
-      CreateCode: readHookBinaryHexFromNS('../build/index'),
+      CreateCode: readHookBinaryHexFromNS('../build/index', 'wasm'),
       Flags: SetHookFlags.hsfOverride,
       HookOn: calculateHookOn(['Invoke']),
       HookNamespace: hexNamespace(namespace),
-      HookApiVersion: 0,
+      HookApiVersion: 3,
+      HookFunctions: [
+        {
+          HookFunction: {
+            FunctionName: convertStringToHex('increment'),
+            FunctionParameters: [
+              TypeParameter('key', 'VL'),
+              TypeParameter('amount', 'UINT8'),
+            ]
+          }
+        },
+        {
+          HookFunction: {
+            FunctionName: convertStringToHex('decrement'),
+            FunctionParameters: [
+              TypeParameter('key', 'VL'),
+              TypeParameter('amount', 'UINT8'),
+            ]
+          }
+        },
+        {
+          HookFunction: {
+            FunctionName: convertStringToHex('queryValue'),
+            Flags: HookFunctionFlags.hffQUERY,
+            FunctionParameters: [
+              TypeParameter('key', 'VL'),
+            ]
+          }
+        }
+      ]
     } as iHook
     await setHooksV3({
       client: testContext.client,
-      seed: testContext.alice.seed,
+      wallet: testContext.alice,
       hooks: [{ Hook: hook }],
     } as SetHookParams)
   })
@@ -48,25 +88,65 @@ describe('test', () => {
     } as iHook
     await clearHookStateV3({
       client: testContext.client,
-      seed: testContext.alice.seed,
+      wallet: testContext.alice,
       hooks: [{ Hook: clearHook }],
     } as SetHookParams)
     await clearAllHooksV3({
       client: testContext.client,
-      seed: testContext.alice.seed,
+      wallet: testContext.alice,
     } as SetHookParams)
     await teardownClient(testContext)
   })
 
-  it('', async () => {
-    const response = await Xrpld.submit(testContext.client, {
+  it('test', async () => {
+    await Xrpld.submit(testContext.client, {
       tx: {
         TransactionType: 'Invoke',
-        Account: testContext.alice.address,
+        Account: testContext.bob.address,
+        Destination: testContext.alice.address,
+        FunctionName: convertStringToHex('increment'),
+        FunctionParameters: [
+          ValueParameter('VL', 'my-value'),
+          ValueParameter('UINT8', 25),
+        ],
       },
-      wallet: testContext.alice,
+      wallet: testContext.bob,
     })
-    console.log(response.meta)
-    expect(response.meta).toHaveProperty('HookExecutions')
+
+    const response1 = await testContext.client.request({
+      command: "hook_query",
+      hook_account: testContext.alice.address,
+      source_account: testContext.alice.address,
+      function_name: 'queryValue',
+      function_params: {
+        key: { type: 'VL', value: convertStringToHex('my-value') },
+      },
+    })
+    console.log(response1.result.query_results)
+
+    await Xrpld.submit(testContext.client, {
+      tx: {
+        TransactionType: 'Invoke',
+        Account: testContext.bob.address,
+        Destination: testContext.alice.address,
+        FunctionName: convertStringToHex('decrement'),
+        FunctionParameters: [
+          ValueParameter('VL', 'my-value'),
+          ValueParameter('UINT8', 3),
+        ],
+      },
+      wallet: testContext.bob,
+    })
+
+    const response2 = await testContext.client.request({
+      command: "hook_query",
+      hook_account: testContext.alice.address,
+      source_account: testContext.alice.address,
+      function_name: 'queryValue',
+      function_params: {
+        key: { type: 'VL', value: convertStringToHex('my-value') },
+      },
+    })
+    console.log(response2.result.query_results)
   })
 })
